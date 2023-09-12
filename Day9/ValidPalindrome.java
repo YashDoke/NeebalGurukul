f9bacb0460d7a18a8e93a06478b1f7b9ab3e8fce
YashDoke <60827184+YashDoke@users.noreply.github.com>
2023-09-13 02:32:15 +0530

    Update and rename ValidPArameters.java to ValidPalindrome.java

--- Day9/ValidPalindrome.java
+++ Day9/ValidPalindrome.java
@@ -11,7 +11,7 @@ package day9;
 
 import java.util.Scanner;
 
-public class ValidPArameters {
+public class ValidPalindrome {
 public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter a String: ");
