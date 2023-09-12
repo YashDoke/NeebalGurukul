07b8c2a2df19bee7a3a036b4e7f82ce8786eff79
YashDoke <60827184+YashDoke@users.noreply.github.com>
2023-09-12 19:28:05 +0530

    Update LegoSetCompetition.java

--- Day5/LegoSetCompetition.java
+++ Day5/LegoSetCompetition.java
@@ -1,107 +1,86 @@
-
-
-package newpackage;
+package practice;
 import java.util.Scanner;
-
 public class LegoSetCompetition {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	outer:do {
+		System.err.println("Welcome to Lego Set Competition ");
+		System.out.println("Enter the name of Lego Set 1");
+		sc.nextLine();
+		String c1= sc.nextLine();
+		System.out.println("Enter the number of pieces in Lego Set 1");
+		int t1= sc.nextInt();
+		System.out.println("Enter the name of Lego Set 2");
+		sc.nextLine();
+		String c2= sc.nextLine();
+		System.out.println("Enter the number of pieces in Lego Set 2");
+		int t2= sc.nextInt();
+		System.out.println("Enter the name of Lego Set 3");
+		sc.nextLine();
+		String c3= sc.nextLine();
+		System.out.println("Enter the number of pieces in Lego Set 3");
+		int t3= sc.nextInt();
+		CompetitionLog p1 = new CompetitionLog(1,"",c1+" "+c2+" "+c3,0);
+		CompetitionLog p2 = new CompetitionLog(2,"",c1+" "+c2+" "+c3,0);
+		int t = t1+t2+t3;
+		int d = 0;
+		
+		while(true) {
+			++d;
+			System.out.println("Enter the number of pieces player 1 used for building on day "+d);
+			int q1 = sc.nextInt();
+			System.out.println("Enter the number of pieces player 2 used for building on day "+d);
+			int q2 = sc.nextInt();
+			
+			p1.setPiecesBuilt(p1.getPiecesBuilt()+q1);
+			p2.setPiecesBuilt(p2.getPiecesBuilt()+q2);
+			
+			if(p1.getPiecesBuilt()>=t1) {
+				p1.setCompleteSets(c1);
+				p1.setIncompleteSets(c1+","+c3);
+			}
+			if(p1.getPiecesBuilt()>=t1+t2) {
+				p1.setCompleteSets(c1+","+c2);
+				p1.setIncompleteSets(c3);
+			}
+			if(p1.getPiecesBuilt()>=t1+t2+t3) {
+				p1.setCompleteSets(c1+","+c2+","+c3);
+				p1.setIncompleteSets("");
+			}
+			
+			if(p2.getPiecesBuilt()>=t1) {
+				p2.setCompleteSets(c1);
+				p2.setIncompleteSets(c1+","+c3);
+			}
+			if(p2.getPiecesBuilt()>=t1+t2) {
+				p2.setCompleteSets(c1+","+c2);
+				p2.setIncompleteSets(c3);
+			}
+			if(p2.getPiecesBuilt()>=t1+t2+t3) {
+				p2.setCompleteSets(c1+","+c2+","+c3);
+				p2.setIncompleteSets("");
+			}
+			
+			if(p1.getPiecesBuilt()>=t&&p2.getPiecesBuilt()>=t) {
+				System.out.println("The competition ended in a tie! There will be a tiebreaker round");
+				continue outer;
+			}
+			else if(p1.getPiecesBuilt()>=t) {
+				System.out.println("Congratulations to player 1 for winning the Lego Set Competition!\n"
+						+ "Additional information about the competition results is below ");
+				System.out.println(p1);
+				System.out.println(p2);
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the Lego Set Competition!");
-
-        CompetitionLog p1 = new CompetitionLog(1, "", "", 0);
-        CompetitionLog p2 = new CompetitionLog(2, "", "", 0);
-        int totalPiecesUsedByPlayer1 = 0;
-        int totalPiecesUsedByPlayer2 = 0;
-        int totalDaysElapsed = 0;
-
-        while (true) {
-            String[] lname = new String[3];
-            int[] npieces = new int[3];
-
-            for (int i = 0; i < 3; i++) {
-                System.out.println("Enter Name of Lego Set " + (i + 1) + ":");
-                lname[i] = sc.nextLine();
-                System.out.println("Enter the number of pieces in Lego Set " + (i + 1) + ":");
-                npieces[i] = sc.nextInt();
-                sc.nextLine();
-            }
-
-            int j = 1;
-            boolean tie = false;
-
-            while (true) {
-                System.out.println("Enter the number of pieces player 1 used for building on day " + (j + totalDaysElapsed));
-                int pieces1 = sc.nextInt();
-                System.out.println("Enter the number of pieces player 2 used for building on day " + (j + totalDaysElapsed));
-                int pieces2 = sc.nextInt();
-
-                p1.setPiecesBuilt(p1.getPiecesBuilt() + pieces1);
-                p2.setPiecesBuilt(p2.getPiecesBuilt() + pieces2);
-
-                if (p1.getPiecesBuilt() >= getTotalPieces(npieces) && p2.getPiecesBuilt() >= getTotalPieces(npieces)) {
-                    tie = true;
-                    break;
-                } else if (p1.getPiecesBuilt() >= getTotalPieces(npieces)) {
-                    handleWinner(p1, p2, lname, npieces, totalPiecesUsedByPlayer1, totalPiecesUsedByPlayer2, j, totalDaysElapsed);
-                    break;
-                } else if (p2.getPiecesBuilt() >= getTotalPieces(npieces)) {
-                    handleWinner(p2, p1, lname, npieces, totalPiecesUsedByPlayer2, totalPiecesUsedByPlayer1, j, totalDaysElapsed);
-                    break;
-                }
-                j++;
-            }
-
-            if (tie) {
-                totalPiecesUsedByPlayer2 += p1.getPiecesBuilt();
-                totalPiecesUsedByPlayer1 += p2.getPiecesBuilt();
-                totalDaysElapsed += j;
-
-                p1.setEqual();
-                p2.setEqual();
-
-                System.out.println("The competition ended in a tie! There will be a tiebreaker round");
-            }
-
-            System.out.println("Do you want to continue (yes/no)?");
-            String choice = sc.nextLine();
-            if (choice.equalsIgnoreCase("no")) {
-                sc.close();
-                return;
-            }
-        }
-    }
-
-    private static int getTotalPieces(int[] npieces) {
-        int total = 0;
-        for (int piece : npieces) {
-            total += piece;
-        }
-        return total;
-    }
-
-    private static void handleWinner(CompetitionLog winner, CompetitionLog loser, String[] lname, int[] npieces, int totalPiecesWonByWinner, int totalPiecesWonByLoser, int j, int totalDaysElapsed) {
-        winner.setCompleteSets(String.join(" ", lname));
-        winner.setIncompleteSets("none");
-
-        String incompleteSets = "";
-        int piecesLeft = winner.getPiecesBuilt();
-        for (int i = 0; i < npieces.length; i++) {
-            if (piecesLeft >= npieces[i]) {
-                piecesLeft -= npieces[i];
-            } else {
-                incompleteSets += lname[i] + " ";
-            }
-        }
-        loser.setCompleteSets("none");
-        loser.setIncompleteSets(incompleteSets);
-
-        System.out.println("Congratulations to player " + winner.getPlayerNumber() + " for winning the Lego Sets Competition!");
-        System.out.println("Additional information about the competition results is below");
-        winner.setPiecesBuilt(totalPiecesWonByWinner);
-        loser.setPiecesBuilt(totalPiecesWonByLoser);
-        System.out.println(winner);
-        System.out.println(loser);
-        System.out.println("The Competition lasted " + (j + totalDaysElapsed) + " days");
-    }
+				break outer;
+			}else if(p2.getPiecesBuilt()>=t) {
+				System.out.println("Congratulations to player 2 for winning the Lego Set Competition!\n"
+						+ "Additional information about the competition results is below ");
+				System.out.println(p1);
+				System.out.println(p2);
+				break outer;
+			}
+		}
+		
+	}while(true);
+}
 }
